cfca02526282c02108d86f738c3956c48545fa5c
Ruhul Amin Parvez <parvez.techforing@gmail.com>
2023-01-27 18:23:00 +0600

    Changed Blog View :question:

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -14,16 +14,24 @@ const GET_BLOGS = gql`
 
 const Blog = () => {
   const { loading, error, data } = useQuery(GET_BLOGS);
+  console.log(data);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   return (
-    <div className="p-10 bg-base-200 flex justify-center items-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {data.blog.map((blog) => (
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <div className="card-body" key={blog.id}>
+    <>
+      <div className="bg-base-200 p-10">
+        <div className="flex justify-end pr-12">
+          <button className="btn btn-primary">Add Blog</button>
+        </div>
+        <h1 className="text-4xl font-bold text-center">Blogs</h1>
+      </div>
+      <div className="bg-base-200 flex justify-center items-center pb-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {data.blog.map((blog) => (
+            <div className="card w-96 bg-base-100 shadow-xl" key={blog.id}>
+              <div className="card-body">
                 <h2 className="card-title">{blog.title}</h2>
                 <p>{blog.description}</p>
                 <div className="card-actions justify-end">
@@ -32,9 +40,9 @@ const Blog = () => {
               </div>
             </div>
           ))}
+        </div>
       </div>
-        
-    </div>
+    </>
   );
 };
 
